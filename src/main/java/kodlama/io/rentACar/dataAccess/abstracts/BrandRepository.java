5f9d5341bd7eab21cc8311fabe19425a5220f201
zehrakonca <49036243+zehrakonca@users.noreply.github.com>
2022-11-08 19:41:15 +0300

    Fifth day coursework.
    Fifth day coursework.

--- src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
+++ src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -1,14 +1,11 @@
 package kodlama.io.rentACar.dataAccess.abstracts;
 
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kodlama.io.rentACar.entities.concretes.Brand;
 
 @Repository
-public interface BrandRepository{
-	List<Brand> getAll();
-	
+public interface BrandRepository extends JpaRepository<Brand, Integer>{
 	
 }
--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,33 +1,28 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="brands")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id; 
+	
+	@Column(name="brandName")
 	private String brandName;
-	
-	public Brand() {
-	}
-
-	public Brand(int id, String brandName) {
-		super();
-		this.id = id;
-		this.brandName = brandName;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getBrandName() {
-		return brandName;
-	}
-
-	public void setBrandName(String brandName) {
-		this.brandName = brandName;
-	}
-	
-	
 }
